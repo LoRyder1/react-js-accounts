8211ae8f5e329e09cc5c91414dba670cf0eb4147
Lovinder Pnag <lspnag@gmail.com>
2016-04-11 14:16:31 -0400

    credit, debit, balanc

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,9 +3,24 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unproccesable_entity
+    end
+  end
+
   def destroy
     @record = Record.find(params[:id])
     # @record.destroy
     head :no_content
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:title, :amount, :date)
   end
 end
