1ce661cdf2a8b2fdb4a26771d3f21fae064927f3
Lovinder Pnag <lspnag@gmail.com>
2016-04-11 15:05:41 -0400

    controller update

--- app/assets/javascripts/components/record_form.js.coffee
+++ app/assets/javascripts/components/record_form.js.coffee
@@ -54,3 +54,4 @@
         className: 'btn btn-primary'
         disabled: !@valid()
         'Create record'
+        
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,6 +3,15 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
   def create
     @record = Record.new(record_params)
 
